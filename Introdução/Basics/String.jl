e1e4d80f90d1f112c350dbfa73a982082f15e798
João Lucas <joaolucas.moreira.moura@gmail.com>
2021-03-30 01:15:32 -0300

    primeira parte do basico introdução julia

---
+++ Introdução/Basics/String.jl
@@ -0,0 +1,9 @@
+##Strings##
+
+#How to get string
+
+s1 = "I am a string."
+
+s2 = """ I am also a string."""
+
+typeof(s2)
---
+++ Introdução/Basics/Variables and Printing.jl
@@ -0,0 +1,32 @@
+##Variables and Printing##
+
+#How to print
+
+println("Lerning Julia")
+
+#How to assing variables
+
+my_answer = 42
+typeof(my_answer)
+
+my_pi = 3.14159
+typeof(my_pi)
+
+my_name = "Julia"
+typeof(my_name)
+
+#How to comment in multi-line
+
+#=
+For multi-line coments,
+use the "#= =#" sequence
+=#
+
+#Syntax for basic math
+
+sum = 3 + 5
+difference = 10 - 3
+product = 20 * 5
+quotient = 100 / 10
+power = 10 ^ 2
+modulus = 101 % 2

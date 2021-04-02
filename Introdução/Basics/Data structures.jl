a266c139692c703867dcbe3fd2e4927bff62b915
João Lucas <joaolucas.moreira.moura@gmail.com>
2021-04-01 23:09:39 -0300

    fim de data structures e inicio control flow

--- Introdução/Basics/Data structures.jl
+++ Introdução/Basics/Data structures.jl
@@ -29,3 +29,43 @@ myphonebook[1]
 
 
 #Tuples
+myfavoriteanimals = ("penguins", "cats", "sugargliders")
+#podemos retornar os itens da tupla deacordo com a ordem
+myfavoriteanimals[1]
+#As tuplas são imutáveis, não se pode acrescentar ou retirar nada
+myfavoriteanimals[1] = "otters"
+
+#Arrays
+#=as arrays são mutaveis, ou seja, é possivel
+alterar seus valores e a ordem segue o padrão
+de uma matriz
+=#
+#array de string
+myfriends = ["Ted", "Robyn", "Barney", "Lily", "Marshall"]
+#também pode sequencia numérica, inteiro
+fibonacci = [1, 1, 2, 3, 5, 8, 13]
+#pode conter inteiros float ou strings no mesmo array ( any )
+mix = [1, 2, 3.0, "hi", 1]
+#podemos escolher elementos distintos do array
+myfriends[3]
+#podemos colocar elementos novos
+myfriends[3] = "Baby Bop"
+myfriends
+# Funções push! e pop!
+#= Também possível editar um array
+usando o push! e o pop!. onde o push!
+adiciona algum item e o pop! remove.
+=#
+push!(fibonacci, 13)
+#Dessa forma o pop! vai remover o último item
+pop!(fibonacci)
+fibonacci
+#também podemos fazer um Array formado por Arrays
+favorites = [["koobideh", "chocolate", "eggs"],["penguins", "cats", "sugargliders"]]
+numbers = [[1, 2, 3], [6, 7, 8, 9]]
+#=podemos criar arrays aleatorios com a função
+rand onde o primeiro argumento são as linhas,
+o segundo as colunas e o terceiro a quantidade
+ de arrays para serem feitos.
+=#
+rand(4, 3, 2)
